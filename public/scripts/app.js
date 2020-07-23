0510d346dcbe0ce9f472ec57eaf8ff4e473abd3a
Mand Cashin <mand.cashin@tfs.com>
2020-07-23 17:52:00 +0100

    chore: import react scripts and install babel

---
+++ public/scripts/app.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var template = React.createElement(
+  'p',
+  null,
+  'test'
+);
+
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
---
+++ src/app.js
@@ -0,0 +1,6 @@
+const template = <p>test</p>;
+
+
+const appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
