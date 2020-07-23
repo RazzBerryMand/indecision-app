82d278fd3d4c2d4ed17c32d7ae709e2c0491d05c
Mand Cashin <mand.cashin@tfs.com>
2020-07-23 18:12:05 +0100

    feat: create templateTwo to test jsx

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,11 +1,33 @@
 'use strict';
 
-var template = React.createElement(
-  'p',
+// const template = (
+//     <div>
+//         <h1>title</h1>
+//         <p>test</p>
+//     </div>
+// );
+
+var templateTwo = React.createElement(
+  'div',
   null,
-  'test'
+  React.createElement(
+    'h1',
+    null,
+    'mand cashin'
+  ),
+  React.createElement(
+    'p',
+    null,
+    'age: 33'
+  ),
+  React.createElement(
+    'p',
+    null,
+    'location: manchester'
+  )
 );
 
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+// ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,6 +1,20 @@
-const template = <p>test</p>;
+// const template = (
+//     <div>
+//         <h1>title</h1>
+//         <p>test</p>
+//     </div>
+// );
+
+const templateTwo = (
+<div>
+<h1>mand cashin</h1>
+<p>age: 33</p>
+<p>location: manchester</p>
+</div>
+);
 
 
 const appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+// ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
