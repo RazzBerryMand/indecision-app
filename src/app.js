29d6b45718b418f416cb2e29d088d4835a2a1aca
Mand Cashin <mand.cashin@tfs.com>
2020-07-28 17:42:58 +0100

    feat: render options to list onSubmit

--- src/app.js
+++ src/app.js
@@ -23,6 +23,7 @@ const removeAll = () => {
 
 const appRoot = document.getElementById("app");
 
+
 const renderOptionsApp = () => {
 
     const template = (
@@ -31,10 +32,12 @@ const renderOptionsApp = () => {
         {app.subtitle && <p>{app.subtitle}</p>}
         <p>{app.options.length > 0 ? "Here are your options" : "No options" }</p>
         <p>{app.options.length}</p>
+
         <button onClick={removeAll}>Remove All</button>
         <ol>
-        <li>Item one</li>
-        <li>Item two</li>
+        {app.options.map((option) => {
+            return <li key={option}>{option}</li>
+            })}
         </ol>
         <form onSubmit={onFormSubmit}>
         <input type="text" name="option"/>
