5a9876e366ec7d1321915ccfcd3b79e26a515baa
Mand Cashin <mand.cashin@tfs.com>
2020-08-04 10:32:34 +0100

    feat: use super to create travellerin es6-classes-1

--- src/playground/es6-classes-1.js
+++ src/playground/es6-classes-1.js
@@ -3,14 +3,60 @@ class Person {
         this.name = name;
         this.age = age;
     }
+    getGreeting() {
+        return `Hi. I am ${this.name}!`;
+    }
     getDescription() {
         return `${this.name} is ${this.age} year(s) old.`;
     }
 }
 
-const me = new Person("Mand Cashin", 33);
-console.log(me.getDescription());
+class Student extends Person {
+constructor(name, age, major) {
+    super(name, age);
+    this.major = major;
+}
+hasMajor() {
+    return !!this.major;
+}
+getDescription() {
+    let description = super.getDescription();
 
-const other = new Person();
-console.log(other.getDescription());
+    if (this.hasMajor()) {
+        description += ` Their major is ${this.major}.`;
+    }
+
+    return description;
+    }
+}
+
+class Traveller extends Person {
+constructor(name, age, homeLocation) {
+    super(name, age);
+    this.homeLocation = homeLocation;
+}
+hasHomeLocation() {
+    return !!this.homeLocation;
+}
+getGreeting() {
+    let greeting = super.getGreeting();
+    
+    if (this.hasHomeLocation()) {
+        greeting += ` I'm visiting from ${this.homeLocation}.`;
+    }
+    return greeting;
+    }
+}
+
+// const me = new Student ("Mand Cashin", 33, "Fine Art");
+// console.log(me.getDescription());
+
+// const other = new Student ();
+// console.log(other.getDescription());
+
+const me = new Traveller ("Mand Cashin", 33, "Manchester");
+console.log(me.getGreeting());
+
+const other = new Traveller ();
+console.log(other.getGreeting());
 
