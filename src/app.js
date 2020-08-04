1228df607d6a5299d3971e7e1e444647b0ae1ece
Mand Cashin <mand.cashin@tfs.com>
2020-08-04 17:59:20 +0100

    feat: add action, options, and addoption classes

--- src/app.js
+++ src/app.js
@@ -1,13 +1,50 @@
 class Header extends React.Component {
   render() {
-    return <p>This is from header</p>;
+    return (
+      <div>
+        <h1>Indecision</h1>
+        <h2>Put your life in the hands of a computer</h2>
+      </div>
+    );
+  }
+}
+
+class Action extends React.Component {
+  render() {
+    return (
+      <div>
+        <button>What should I do?</button>
+      </div>
+    );
+  }
+}
+
+class Options extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Options here</h1>
+      </div>
+    );
+  }
+}
+
+class AddOption extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Add Option here</h1>
+      </div>
+    );
   }
 }
 
 const jsx = (
   <div>
-    <h1>Title</h1>
     <Header />
+    <Action />
+    <Options />
+    <AddOption />
   </div>
 );
 
