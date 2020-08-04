06fead6690e633a07704dce5a0bae9f2085be4c3
Mand Cashin <mand.cashin@tfs.com>
2020-08-04 10:37:26 +0100

    chore: run prettier and es lint on all code

--- src/playground/es6-classes-1.js
+++ src/playground/es6-classes-1.js
@@ -1,51 +1,51 @@
 class Person {
-    constructor(name = "Anonymous", age = 0) {
-        this.name = name;
-        this.age = age;
-    }
-    getGreeting() {
-        return `Hi. I am ${this.name}!`;
-    }
-    getDescription() {
-        return `${this.name} is ${this.age} year(s) old.`;
-    }
+  constructor(name = "Anonymous", age = 0) {
+    this.name = name;
+    this.age = age;
+  }
+  getGreeting() {
+    return `Hi. I am ${this.name}!`;
+  }
+  getDescription() {
+    return `${this.name} is ${this.age} year(s) old.`;
+  }
 }
 
 class Student extends Person {
-constructor(name, age, major) {
+  constructor(name, age, major) {
     super(name, age);
     this.major = major;
-}
-hasMajor() {
+  }
+  hasMajor() {
     return !!this.major;
-}
-getDescription() {
+  }
+  getDescription() {
     let description = super.getDescription();
 
     if (this.hasMajor()) {
-        description += ` Their major is ${this.major}.`;
+      description += ` Their major is ${this.major}.`;
     }
 
     return description;
-    }
+  }
 }
 
 class Traveller extends Person {
-constructor(name, age, homeLocation) {
+  constructor(name, age, homeLocation) {
     super(name, age);
     this.homeLocation = homeLocation;
-}
-hasHomeLocation() {
+  }
+  hasHomeLocation() {
     return !!this.homeLocation;
-}
-getGreeting() {
+  }
+  getGreeting() {
     let greeting = super.getGreeting();
-    
+
     if (this.hasHomeLocation()) {
-        greeting += ` I'm visiting from ${this.homeLocation}.`;
+      greeting += ` I'm visiting from ${this.homeLocation}.`;
     }
     return greeting;
-    }
+  }
 }
 
 // const me = new Student ("Mand Cashin", 33, "Fine Art");
@@ -54,9 +54,8 @@ getGreeting() {
 // const other = new Student ();
 // console.log(other.getDescription());
 
-const me = new Traveller ("Mand Cashin", 33, "Manchester");
+const me = new Traveller("Mand Cashin", 33, "Manchester");
 console.log(me.getGreeting());
 
-const other = new Traveller ();
+const other = new Traveller();
 console.log(other.getGreeting());
-
