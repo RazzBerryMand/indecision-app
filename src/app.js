d54950df8bb6350e461e343062cba490b5d960d7
Mand Cashin <mand.cashin@tfs.com>
2020-08-10 14:13:27 +0100

    feat: add some button functionality to app.js

--- src/app.js
+++ src/app.js
@@ -27,19 +27,26 @@ class Header extends React.Component {
 }
 
 class Action extends React.Component {
+  handlePick() {
+    alert("button clicked");
+  }
   render() {
     return (
       <div>
-        <button>What should I do?</button>
+        <button onClick={this.handlePick}>What should I do?</button>
       </div>
     );
   }
 }
 
 class Options extends React.Component {
+  removeAll() {
+    alert("button clicked");
+  }
   render() {
     return (
       <div>
+        <button onClick={this.removeAll}>Remove All</button>
         {this.props.options.map((option) => (
           <Option key={option} optionText={option} />
         ))}
@@ -55,10 +62,22 @@ class Option extends React.Component {
 }
 
 class AddOption extends React.Component {
+  addOption(e) {
+    e.preventDefault();
+
+    const option = e.target.elements.option.value.trim();
+
+    if (option) {
+      alert(option);
+    }
+  }
   render() {
     return (
       <div>
-        <h1>Add Option here</h1>
+        <form onSubmit={this.addOption}>
+          <input type="text" name="option" />
+          <button>Add Option</button>
+        </form>
       </div>
     );
   }
