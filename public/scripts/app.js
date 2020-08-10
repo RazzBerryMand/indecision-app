e08112f77e1f4080261489f6d93ed6d3d439f985
Mand Cashin <mand.cashin@tfs.com>
2020-08-10 16:18:59 +0100

    feat: use setState and prevState to add functionality to addone and minusone

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -19,18 +19,30 @@ var Counter = function (_React$Component) {
     _this.handleAddOne = _this.handleAddOne.bind(_this);
     _this.handleMinusOne = _this.handleMinusOne.bind(_this);
     _this.handleReset = _this.handleReset.bind(_this);
+
+    _this.state = {
+      count: 0
+    };
     return _this;
   }
 
   _createClass(Counter, [{
     key: "handleAddOne",
     value: function handleAddOne() {
-      alert("add one");
+      this.setState(function (prevState) {
+        return {
+          count: prevState.count + 1
+        };
+      });
     }
   }, {
     key: "handleMinusOne",
     value: function handleMinusOne() {
-      alert("minus one");
+      this.setState(function (prevState) {
+        return {
+          count: prevState.count - 1
+        };
+      });
     }
   }, {
     key: "handleReset",
@@ -46,7 +58,8 @@ var Counter = function (_React$Component) {
         React.createElement(
           "h1",
           null,
-          "Count:"
+          "Count: ",
+          this.state.count
         ),
         React.createElement(
           "button",
--- src/playground/counter-example.js
+++ src/playground/counter-example.js
@@ -4,14 +4,26 @@ class Counter extends React.Component {
     this.handleAddOne = this.handleAddOne.bind(this);
     this.handleMinusOne = this.handleMinusOne.bind(this);
     this.handleReset = this.handleReset.bind(this);
+
+    this.state = {
+      count: 0,
+    };
   }
 
   handleAddOne() {
-    alert("add one");
+    this.setState((prevState) => {
+      return {
+        count: prevState.count + 1,
+      };
+    });
   }
 
   handleMinusOne() {
-    alert("minus one");
+    this.setState((prevState) => {
+      return {
+        count: prevState.count - 1,
+      };
+    });
   }
 
   handleReset() {
@@ -21,7 +33,7 @@ class Counter extends React.Component {
   render() {
     return (
       <div>
-        <h1>Count:</h1>
+        <h1>Count: {this.state.count}</h1>
         <button onClick={this.handleAddOne}>+1</button>
         <button onClick={this.handleMinusOne}>-1</button>
         <button onClick={this.handleReset}>reset</button>
