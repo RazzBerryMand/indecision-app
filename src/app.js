9966b6c4816b875bda56f2fdf5baf60007a0ef17
Mand Cashin <mand.cashin@tfs.com>
2020-08-13 13:28:59 +0100

    chore: install react and react-dom

--- src/app.js
+++ src/app.js
@@ -1,12 +1,5 @@
-import subtract, { square, add } from "./utils";
-import isSenior, { isAdult, canDrink } from "./person";
+import React from "react";
+import ReactDOM from "react-dom";
 
-console.log("app is running!!!!!!!!!");
-
-console.log(square(4));
-console.log(add(2, 2));
-console.log(subtract(100, 50));
-
-console.log(isAdult(33));
-console.log(canDrink(33));
-console.log(isSenior(33));
+const template = React.createElement("p", {}, "testing 123");
+ReactDOM.render(template, document.getElementById("app"));
