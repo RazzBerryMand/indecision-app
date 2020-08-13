2b1223959b4bd627be7e5de4e82b3b309b9d49e2
Mand Cashin <mand.cashin@tfs.com>
2020-08-13 17:17:04 +0100

    feat: add optionModal

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # indecision-app
+
 React js app that makes a choice at random
+
+Built following the complete react developer course (udemy)
--- src/app.js
+++ src/app.js
@@ -3,11 +3,3 @@ import ReactDOM from "react-dom";
 import IndecisionApp from "./components/IndecisionApp";
 
 ReactDOM.render(<IndecisionApp />, document.getElementById("app"));
-
-class NewSyntax {
-  name = "Mand";
-}
-
-const newSyntax = new NewSyntax();
-
-console.log(NewSyntax);
